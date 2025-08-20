6db4369b5953b18130a639fe178a8ef6eb054ae5
ChandanLabs <chandan.kumar.sah.teli2005@gmail.com>
2025-08-21 00:54:45 +0530

    Function topic practice code added

---
+++ Functions/OddEven.java
@@ -0,0 +1,21 @@
+package Functions;
+import java.util.*;
+
+public class OddEven {
+    static void CheckOddEvenNum(int n){
+        if(n % 2 ==0){
+            System.out.println(+n+" is a even number");
+        }
+        else{
+            System.out.println(+n+" is a Odd number");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int num = inp.nextInt();
+        CheckOddEvenNum(num);
+
+
+    }
+}
---
+++ Functions/PrimeCheck.java
@@ -0,0 +1,21 @@
+package Functions;
+import java.util.*;
+
+
+public class PrimeCheck {
+    static void CheckPrimeNum(int n){
+        if(n % 2==1){
+            System.out.println(n+" is a prime number");
+        }
+        else{
+            System.out.println(n+" is not a prime number");
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int num = inp.nextInt();
+        CheckPrimeNum(num);
+    }
+}
