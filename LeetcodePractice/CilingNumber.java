23d62ea42b33a80933de4c76de553f96b40256f4
ChandanLabs <chandan.kumar.sah.teli2005@gmail.com>
2025-09-07 18:37:50 +0530

    A example of Ciling and floor

---
+++ LeetcodePractice/CilingNumber.java
@@ -0,0 +1,27 @@
+public class CilingNumber{
+    public static void main(String[] args) {
+        int[] arr = {2,3,5,9,13,16,18};
+        int target = 14;
+        int ans = Solution(arr,target);
+        System.out.println(ans); 
+    }
+    static int Solution(int[] arr , int target){
+        int start = 0 ;
+        int end = arr.length -1 ;
+        while (start <= end){
+                int mid = start + (end - start) / 2;
+                // int element = arr[mid];
+                if(target == arr[mid]){
+                    return arr[mid];
+                }
+                else if(target < arr[mid]){
+                    end = mid - 1 ;
+                }
+                else if(target > arr[mid]){
+                    start = mid + 1 ;
+                }
+              
+        }
+        return start;
+    }
+}
