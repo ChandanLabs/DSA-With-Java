b9e7715cf909d9ddb8d3053dc6195aef8bb2f5fd
ChandanLabs <chandan.kumar.sah.teli2005@gmail.com>
2025-09-21 22:03:13 +0530

    initial commit

---
+++ MathDSA/Anagram.java
@@ -0,0 +1,21 @@
+package MathDSA;
+
+import java.util.Arrays;
+
+public class Anagram {
+    public static void main(String[] args) {
+        
+    }
+    static boolean isAnagram(String s1,String s2){
+        if(s1.length() != s2.length()){
+            return false;
+        }
+        char[] a = s1.toCharArray();
+        char[] b = s2.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return Arrays.equals(a, b);
+    }
+}
