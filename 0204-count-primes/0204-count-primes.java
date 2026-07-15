0b259065de9a675b95aa7296a9a1f6de4885de1e
Chandan Kumar Sah Teli <chandan.kumar.sah.teli2005@gmail.com>
2026-07-15 10:41:57 +0530

    Time: 96 ms (75.07%), Space: 51.3 MB (38.11%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int countPrimes(int n) {
+        if(n < 2){
+            return 0;
+        }
+        boolean isPrime[] = new boolean[n];
+
+        Arrays.fill(isPrime, true);
+
+        int sqrtn = (int)Math.sqrt(n);
+        for(int i = 2; i <= sqrtn; i++){
+            if(isPrime[i]){
+                for(int j = i * i; j < n; j+=i){
+                    isPrime[j] = false;
+                }
+            }
+        }
+            int count = 0;
+            for(int i = 2; i < n; i++){
+                if(isPrime[i]){
+                    count++;
+                    // return count;
+                }
+            }
+        
+        return count;
+    }
+}
