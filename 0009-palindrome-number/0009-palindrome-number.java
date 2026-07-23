f7853bb99bc723cefce26ef6e7b6700528024eda
Chandan Kumar Sah Teli <chandan.kumar.sah.teli2005@gmail.com>
2026-07-23 10:00:15 +0530

    Time: 4 ms (100%), Space: 46 MB (33.75%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        int temp = x;
+        int n = 0;
+        while( x > 0){
+            int rem = x % 10;
+            n = rem + n * 10;
+            x = x/10;
+        }
+        if(temp == n){
+            return true;
+        }
+        return false;
+    }
+}
