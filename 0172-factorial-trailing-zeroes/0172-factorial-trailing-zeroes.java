d1e4571b17c5187b036e7bc9bb712597d33b4b75
Chandan Kumar Sah Teli <chandan.kumar.sah.teli2005@gmail.com>
2026-07-23 09:51:12 +0530

    Time: 3 ms (3.95%), Space: 42.3 MB (73.8%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -1,9 +1,9 @@
 class Solution {
     public int trailingZeroes(int n) {
-        // int fact = 1;
-        // for(int i = 2; i <= n;  i++){
-        //     fact = fact * i;
-        // }
+        int fact = 1;
+        for(int i = 2; i <= n;  i++){
+            fact = fact * i;
+        }
 
          
         int count = 0;
@@ -13,6 +13,11 @@ class Solution {
         }
         return count;
          
+        // int count = 0;
+        // for(int i = 5; i * i <= n; i*=5){
+        //     count = count + n / i;
 
+        // }
+        // return count;
     }
 }
