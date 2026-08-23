22894b6be69d3037667bab62b7b18875453e7502
Chandan Kumar Sah Teli <chandan.kumar.sah.teli2005@gmail.com>
2026-08-23 23:43:35 +0530

    Time: 8 ms (53.28%), Space: 46.9 MB (64.23%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int half = n / 2;
+
+        int leftSum = 0;
+        int rightSum = 0;
+        int leftQ = 0;
+        int rightQ = 0;
+
+        for (int i = 0; i < half; i++) {
+            if (num.charAt(i) == '?') {
+                leftQ++;
+            } else {
+                leftSum += num.charAt(i) - '0';
+            }
+        }
+
+        for (int i = half; i < n; i++) {
+            if (num.charAt(i) == '?') {
+                rightQ++;
+            } else {
+                rightSum += num.charAt(i) - '0';
+            }
+        }
+
+        if ((leftQ + rightQ) % 2 == 1) {
+            return true;
+        }
+
+        int sumDiff = leftSum - rightSum;
+        int questionDiff = leftQ - rightQ;
+
+        return sumDiff + (questionDiff / 2) * 9 != 0;
+    }
+}
