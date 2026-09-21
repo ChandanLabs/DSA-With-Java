81dcaf514bbe78c8d6737120e03732f3f7cb30fe
Chandan Kumar Sah Teli <chandan.kumar.sah.teli2005@gmail.com>
2026-09-21 17:12:48 +0530

    Time: 3 ms (93.92%), Space: 44.6 MB (59.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,6 +2,8 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         int[] freq = new int[26];
 
+        if(s.length() != t.length()) return false;
+
         for(char ch : s.toCharArray()){
             if(ch >= 'a' && ch <= 'z'){
                 freq[ch - 'a']++;
